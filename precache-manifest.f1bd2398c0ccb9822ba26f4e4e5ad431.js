61658430dc63aaa2485c94a61861d8507651a55a
David Lui <dvidlui@gmail.com>
2018-11-02 03:00:17 +0800

    Updates

---
+++ precache-manifest.f1bd2398c0ccb9822ba26f4e4e5ad431.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "d6d0aa42199878ab9859",
+    "url": "/react-tutorial/static/js/runtime~main.d6d0aa42.js"
+  },
+  {
+    "revision": "98a769132651dada0e5d",
+    "url": "/react-tutorial/static/js/main.98a76913.chunk.js"
+  },
+  {
+    "revision": "5b966dc2e69f15274453",
+    "url": "/react-tutorial/static/js/1.5b966dc2.chunk.js"
+  },
+  {
+    "revision": "98a769132651dada0e5d",
+    "url": "/react-tutorial/static/css/main.fb8d00dc.chunk.css"
+  },
+  {
+    "revision": "062884192a031de355cc776f7769396f",
+    "url": "/react-tutorial/index.html"
+  }
+];
